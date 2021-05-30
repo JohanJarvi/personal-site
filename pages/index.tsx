91abf1fbf326659570bad8faffeef3a82c823a14
Johan Jarvi <johan.jarvi@gmail.com>
2021-05-30 12:27:07 +1000

    PS13: Implement testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ export default function Home() {
       <h1 className={styles.title}>johan.</h1>
       <h2 className={styles.subtitle}>I am a second heading.</h2>
       <p>I am a paragraph.</p>
+      <p>I am a paragraph.</p>
     </div>
   );
 }
