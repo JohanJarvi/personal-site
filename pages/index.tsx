bbe5aed82720381d1bd2cfc56c9d09aaea22067d
Johan Jarvi <johan.jarvi@gmail.com>
2021-05-30 11:26:49 +1000

    PS10: Add fonts and scss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
+import styles from "../styles/Home.module.scss"
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello world!</h1>
-      <h2>I am a second heading.</h2>
+      <h1 className={styles.title}>johan.</h1>
+      <h2 className={styles.subtitle}>I am a second heading.</h2>
       <p>I am a paragraph.</p>
     </div>
   );
