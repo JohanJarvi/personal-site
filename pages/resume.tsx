9e78f15244068b269d919ac696ff4150cafc292b
Johan Jarvi <johan.jarvi@gmail.com>
2021-06-05 10:46:40 +1000

    PS-14: Add blank page for resume

---
+++ pages/resume.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBar from "../components/NavBar/NavBar";
+
+export default function Resume() {
+  return (
+    <div>
+      <NavBar links={["johan.", "about.", "resume.", "blog."]}></NavBar>
+      <h1>resume.</h1>
+    </div>
+  );
+}
