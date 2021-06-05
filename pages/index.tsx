831fa1f71f2758e4ec7429ae96cce4c1952cbb9e
Johan Jarvi <johan.jarvi@gmail.com>
2021-06-05 12:04:45 +1000

    PS-15: Create the blurb component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,23 @@
 import React from "react";
+import Blurb from "../components/Blurb/Blurb";
 import Header from "../components/Header/Header";
 import NavBar from "../components/NavBar/NavBar";
-import styles from "../styles/Home.module.scss";
+import styles from "../styles/index.module.scss";
 
 export default function Home() {
   return (
     <div>
-      <NavBar links={["johan.", "about.", "resume.", "blog."]}></NavBar>
-      <Header title="johan."></Header>
-      <h2 className={styles.subtitle}>I am a second heading.</h2>
-      <p>I am a paragraph.</p>
-      <p>I am a paragraph.</p>
+      <NavBar links={["johan", "about", "resume", "blog"]} />
+      <div className={styles.container}>
+        <div>
+          <Header title="Johan" />
+          <Blurb
+            content={`A fullstack Software Engineer and lover of surf and the ocean.
+      I possess a wide range of skills in mobile, web, robotics and databases.`}
+          />
+        </div>
+        <img className={styles.ocean} src="/me_surf.jpeg" />
+      </div>
     </div>
   );
 }
