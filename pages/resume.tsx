3b7412d92106915b3687c7e7c15a398eaa15de99
Johan Jarvi <johan.jarvi@gmail.com>
2021-12-09 13:42:02 +1000

    PS-17: Create the second resume card

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -23,6 +23,79 @@ export default function Resume() {
     setWidth(window.innerWidth);
   }, []);
 
+  const getFirstCardContent = () => {
+    return (
+      <React.Fragment>
+        <Typography variant="subtitle2" color="text.secondary">
+          Feb 2021 - Jan 2022
+        </Typography>
+        <Typography variant="h5">
+          <strong>Australian Droid + Robot</strong>
+        </Typography>
+        <Typography>
+          <br />
+          When I worked for ADR they were a small robotics startup in the city
+          where my day to day software responsibilities would vary greatly. From
+          Web development to robotics to Android App development.
+          <br />
+          <br />
+          Key achievements included:
+          <ul>
+            <li>
+              Achieving <em>real-time</em> video and robot control using{" "}
+              <strong>WebRTC</strong> with JavaScript and Android
+            </li>
+            <li>
+              Developing a full microservice solution using FastAPI in Python
+              together with a React Front End in order to monitor{" "}
+              <strong>live LiDAR data</strong> as well as manipulate robot
+              behaviour
+            </li>
+          </ul>
+        </Typography>
+      </React.Fragment>
+    );
+  };
+
+  const getSecondCardContent = () => {
+    return (
+      <React.Fragment>
+        <Typography variant="subtitle2" color="text.secondary">
+          May 2019 - Feb 2021
+        </Typography>
+        <Typography variant="h5">
+          <strong>Ambrose Building Pty Ltd</strong>
+        </Typography>
+        <Typography>
+          <br />
+          At Ambrose building I was mostly focused on maintaining and creating
+          Angular driven web applications to make the life of the other
+          employees easier including a PWA for use by the Tradies and a
+          scheduling application used by the Building Supervisors along with
+          many more.
+          <br />
+          <br />
+          Key achievements included:
+          <ul>
+            <li>
+              Developing a recursive search algorithm that was used in
+              production heavily every single day and performed its job
+              effectively and efficiently
+            </li>
+            <li>
+              Being named App Development Top Performer for 2020 on the final
+              award ceremony I attended prior to leaving
+            </li>
+            <li>
+              Mentoring Junior employees who went on to have careers at Ambrose;
+              some that are still there today I believe.
+            </li>
+          </ul>
+        </Typography>
+      </React.Fragment>
+    );
+  };
+
   return (
     <div>
       <NavBar links={["johan", "resume", "blog"]} />
@@ -35,42 +108,7 @@ export default function Resume() {
             </TimelineSeparator>
             <TimelineContent>
               <Card sx={{ display: "inline-block" }}>
-                <CardContent>
-                  <Typography variant="subtitle2" color="text.secondary">
-                    Feb 2020 - Present
-                  </Typography>
-                  <Typography variant="h5">
-                    <strong>Australian Droid + Robot</strong>
-                  </Typography>
-                  <Typography>
-                    <br />
-                    When I worked for ADR they were a small robotics startup in
-                    the city where my day to day software responsibilities would
-                    vary greatly. From Web development to robotics to Android
-                    App development.
-                    <br />
-                    <br />
-                    Key achievements included:
-                    <ul>
-                      <li>
-                        Achieving <em>real-time</em> video and robot control
-                        using <strong>WebRTC</strong> with JavaScript and
-                        Android
-                      </li>
-                      <li>
-                        Developing a full microservice solution using FastAPI in
-                        Python together with a React Front End in order to
-                        monitor <strong>live LiDAR data</strong> as well as
-                        manipulate robot behaviour
-                      </li>
-                    </ul>
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button variant="outlined" color="inherit">
-                    Learn More
-                  </Button>
-                </CardActions>
+                <CardContent>{getFirstCardContent()}</CardContent>
               </Card>
             </TimelineContent>
           </TimelineItem>
@@ -80,8 +118,8 @@ export default function Resume() {
               <TimelineConnector sx={{ bgcolor: "primary.main" }} />
             </TimelineSeparator>
             <TimelineContent>
-              <Card>
-                <CardContent>Hello world?</CardContent>
+              <Card sx={{ textAlign: "left" }}>
+                <CardContent>{getSecondCardContent()}</CardContent>
               </Card>
             </TimelineContent>
           </TimelineItem>
